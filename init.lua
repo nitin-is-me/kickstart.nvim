f43f7f2bf3f7a37084d8ce7c4c57bdbdd2bca2e9
Nitin <150040530+nitin-is-me@users.noreply.github.com>
2025-04-12 08:18:22 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -414,7 +414,7 @@ require('lazy').setup({
 
       -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
-      pcall(require('telescope').load_extension, 'ui-select')
+      --pcall(require('telescope').load_extension, 'ui-select')
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
