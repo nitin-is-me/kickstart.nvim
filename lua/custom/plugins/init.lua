6b4e68b0fb3dacb05870e2f3d530faf8e6a99af5
Nitin <150040530+nitin-is-me@users.noreply.github.com>
2025-04-12 07:02:57 +0530

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,13 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+
+ { "akinsho/toggleterm.nvim", config = function()
+    require("toggleterm").setup({
+      -- Your custom configuration options for toggleterm
+      open_mapping = "<C-t>",  -- Default shortcut to open the terminal (ctrl + t)
+      direction = "horizontal",       -- You can change the terminal's direction (float, horizontal, vertical, etc.)
+    })
+  end },
+}
