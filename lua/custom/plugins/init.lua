285a39101df84b40a341f9625e63225355668bd2
Nitin <150040530+nitin-is-me@users.noreply.github.com>
2025-04-12 07:35:13 +0530

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,20 +4,10 @@
 -- See the kickstart.nvim README for more information
 return {
 
- 
  { "akinsho/toggleterm.nvim", config = function()
   require("toggleterm").setup({
-    open_mapping = "<C-t>",  -- Your keybinding for opening the terminal
-    direction = "horizontal",        -- Floating terminal
-    on_open = function(term)
-      vim.cmd("startinsert")  -- Automatically start in insert mode
-      -- Disable mouse in terminal
-      vim.cmd("setlocal mouse=")  -- Disable mouse in terminal
-    end,
-    on_close = function(term)
-      -- Optionally, you can set back the mouse control when terminal is closed
-      vim.cmd("setlocal mouse=a")  -- Enable mouse control for other buffers
-    end,
+    open_mapping = "<C-t>",  -- Use Ctrl+t to toggle the terminal
+    direction = "horizontal",     -- Floating terminal
   })
 end },
 }
