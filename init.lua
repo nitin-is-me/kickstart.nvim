f013e011b057f45c1d6e50a21a0f3022e199371b
Nitin <150040530+nitin-is-me@users.noreply.github.com>
2025-04-13 15:50:29 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -90,6 +90,12 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Use 2 spaces for shiftwidth
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
