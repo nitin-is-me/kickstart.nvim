59bfd3230e34a72cd586d9fdd420b7f4ff6a2600
Nitin <150040530+nitin-is-me@users.noreply.github.com>
2025-04-22 22:04:16 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -818,7 +818,7 @@ require('lazy').setup({
         -- <c-k>: Toggle signature help
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'enter',
+        preset = 'default',
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
