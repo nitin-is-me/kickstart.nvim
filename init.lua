6891842721eead5dbcb44e772344716f767b108e
Nitin <150040530+nitin-is-me@users.noreply.github.com>
2025-04-22 21:27:12 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -241,6 +241,13 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
+  -- To auto-close brackets
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    opts = {} -- this is equivalent to setup({}) function
+  },
+    
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
